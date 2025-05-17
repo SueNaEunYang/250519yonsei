6d2529c331e567514d264ffd78d9f9ff8fa643fb
SueCream <artisticsue@naver.com>
2025-05-17 18:39:51 +0900

    add 13-2, fix script.js

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "slide11.html",
     "slide12.html",
     "slide13.html",
+    "slide13-2.html",
     "slide14.html",
     "slide15.html",
     "slide16.html",
